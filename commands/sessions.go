3d37811486b5b93fbf65475c697060571ef96009
Khorevaa <khorevaa@gmail.com>
2017-12-07 08:05:04 +0300

    команда unlock

--- commands/sessions.go
+++ commands/sessions.go
@@ -60,85 +60,7 @@ func (_ SessionsUnLock) Name() string { return "unlock ul" }
 func (_ SessionsUnLock) Desc() string {
 	return "Снять блокировку соединений"
 }
-
 func (c SessionsUnLock) Init(config config.Config) func(*cli.Cmd) {
-
-	return func(cmd *cli.Cmd) {
-		// These are the command specific options and args, nicely scoped inside a func
-
-		for _, subCommand := range c.subCommands {
-			cmd.Command(subCommand.Name(), subCommand.Desc(), subCommand.Init(config))
-		}
-
-		lockStart := Duration(0)
-		var (
-			dbUser    = cmd.StringOpt("db-user w", "Администратор", "Пользователь информационной базы")
-			dbPwd     = cmd.StringOpt("db-pwd p", "", "Пароль пользователя информационной базы")
-			ucCode    = cmd.StringOpt("uc-code uc", "", "Ключ разрешения запуска")
-			rac       = cmd.StringOpt("rac r", "localhost:1545", "Сетевой адрес RAS")
-			updateDir = cmd.StringArg("CONNECT", "", "Строка подключения к информационной базе")
-
-			lockMessage = cmd.StringOpt("lock-message m", "", "Ключ разрешения запуска")
-		)
-
-		cmd.VarOpt("lock-start s", &lockStart, "Время старта блокировки пользователей, время указываем как '2040-12-31T23:59:59")
-		cmd.VarOpt("lock-end s", &lockStart, "Время старта блокировки пользователей, время указываем как '2040-12-31T23:59:59")
-
-		logUpdate := config.Log().NewContextLogger(logging.LogFeilds{
-			"command": "update",
-		})
-
-		// What to run when this command is called
-		cmd.Action = func() {
-			// Inside the action, and only inside, you can safely access the values of the options and arguments
-
-			Обновлятор := update.НовоеОбновление(*db, *dbUser, *dbPwd, *ucCode)
-			Обновлятор.УстановитьВерсиюПлатформы(config.V8)
-			Обновлятор.ФайлОбновления = *updateDir
-			Обновлятор.ВыполнитьЗагрузкуВместоОбновения = *loadCf
-			Обновлятор.УстановитьЛог(logUpdate)
-			workErr := Обновлятор.ВыполнитьОбновление()
-
-			if workErr != nil {
-				logUpdate(logging.LogFeilds{
-					"db":        *db,
-					"updateDir": *updateDir,
-					"loadCf":    *loadCf,
-					"ucCode":    *ucCode,
-					"v8":        config.V8,
-				}).WithError(workErr).Error("Ошибка выполнения команды: ")
-			}
-
-		}
-		//cmd.Spec = "[-l --uc -u -p]"
-	}
-
-}
-
-// Declare your type
-type Duration time.Duration
-
-// Make it implement flag.Value
-func (d *Duration) Set(v string) error {
-	parsed, err := time.ParseDuration(v)
-	if err != nil {
-		return err
-	}
-	*d = Duration(parsed)
-	return nil
-}
-
-func (d *Duration) String() string {
-	duration := time.Duration(*d)
-	return duration.String()
-}
-
-func (_ SessionsLock) Name() string { return "lock l" }
-func (_ SessionsLock) Desc() string {
-	return "Установить блокировку соединений"
-}
-
-func (c SessionsLock) Init(config config.Config) func(*cli.Cmd) {
 
 	sessionsInit := func(cmd *cli.Cmd) {
 		// These are the command specific options and args, nicely scoped inside a func
@@ -188,33 +110,87 @@ func (c SessionsLock) Init(config config.Config) func(*cli.Cmd) {
 	return sessionsInit
 }
 
-func init() {
-	RootCmd.AddCommand(sessionsCmd)
+func (c SessionsLock) Init(config config.Config) func(*cli.Cmd) {
 
-	sessionsCmd.AddCommand(lockDBCmd)
-	sessionsCmd.AddCommand(unlockDBCmd)
-	// Here you will define your flags and configuration settings.
-	sessionsCmd.PersistentFlags().StringP("ras", "r", "localhost:1545", "Сетевой адрес RAS")
+	return func(cmd *cli.Cmd) {
+		// These are the command specific options and args, nicely scoped inside a func
 
-	sessionsCmd.PersistentFlags().StringP("db", "c", "", "Строка подключения к информационной базе")
-	sessionsCmd.PersistentFlags().StringP("db-user", "u", "", "Пользователь информационной базы")
-	sessionsCmd.PersistentFlags().StringP("db-pwd", "p", "", "Пароль пользователя информационной базы")
+		for _, subCommand := range c.subCommands {
+			cmd.Command(subCommand.Name(), subCommand.Desc(), subCommand.Init(config))
+		}
 
-	sessionsCmd.PersistentFlags().StringP("cluster-admin", "", "", "Администратор кластера")
-	sessionsCmd.PersistentFlags().StringP("cluster-pwd", "", "", "Пароль администратора кластера")
+		lockStart := Duration(0)
+		lockEnd := Duration(0)
+		var (
+			dbUser       = cmd.StringOpt("db-user w", "Администратор", "Пользователь информационной базы")
+			dbPwd        = cmd.StringOpt("db-pwd p", "", "Пароль пользователя информационной базы")
+			ucCode       = cmd.StringOpt("uc-code uc", "", "Ключ разрешения запуска")
+			clusterAdmin = cmd.StringArg("cluster-admin ca", "", "Администратор кластера")
+			clusterPwd   = cmd.StringArg("cluster-pwd cp", "", "Пароль администратора кластера")
+			lockMessage  = cmd.StringOpt("lock-message m", "Выполняются технические работы. Установлена блокировка сеансов.", "Ключ разрешения запуска")
+			rasRunMode   = cmd.StringArg("ras-run rr", "noRun", "Режим запуска RAS (noRun, run, stop")
+			ras          = cmd.StringArg("RAS", "localhost:1545", "Сетевой адрес RAS")
+			db           = cmd.StringArg("CONNECT", "", "Строка подключения к информационной базе")
+		)
 
-	lockDBCmd.Flags().StringP("lock-message", "", "", "Сообщение облокировки")
-	lockDBCmd.Flags().StringP("lock-uc-code", "", "", "Ключ разрешения запуска")
-	lockDBCmd.Flags().StringP("lock-start", "", "", "Время старта блокировки пользователей, время указываем как '2040-12-31T23:59:59'")
-	lockDBCmd.Flags().StringP("lock-started", "", "", "Время старта блокировки через n сек")
+		cmd.VarOpt("lock-start s", &lockStart, "Время старта блокировки пользователей, время указываем как '2040-12-31T23:59:59")
+		cmd.VarOpt("lock-end s", &lockEnd, "Время старта блокировки пользователей, время указываем как '2040-12-31T23:59:59")
 
-	unlockDBCmd.Flags().StringP("uc-code", "", "", "Ключ разрешения запуска")
+		logCommand := config.Log().NewContextLogger(logging.LogFeilds{
+			"command":    "session",
+			"subcommand": "lock",
+		})
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// sessionsCmd.PersistentFlags().String("foo", "", "A help for foo")
+		// What to run when this command is called
+		cmd.Action = func() {
+			// Inside the action, and only inside, you can safely access the values of the options and arguments
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// sessionsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+			Обновлятор := update.НовоеОбновление(*db, *dbUser, *dbPwd, *ucCode)
+			Обновлятор.УстановитьВерсиюПлатформы(config.V8)
+			Обновлятор.УстановитьЛог(logCommand)
+			workErr := Обновлятор.ВыполнитьОбновление()
+
+			if workErr != nil {
+				logCommand(logging.LogFeilds{
+					"db":           *db,
+					"dbUser":       *dbUser,
+					"ucCode":       *ucCode,
+					"v8":           config.V8,
+					"ras":          *ras,
+					"rasRunMode":   *rasRunMode,
+					"lockMessage":  *lockMessage,
+					"lockStart":    lockStart,
+					"lockEnd":      lockEnd,
+					"clusterAdmin": clusterAdmin,
+					"clusterPwd":   clusterPwd,
+				}).WithError(workErr).Error("Ошибка выполнения команды: ")
+			}
+
+		}
+		//cmd.Spec = "[-l --uc -u -p]"
+	}
+
+}
+
+// Declare your type
+type Duration time.Duration
+
+// Make it implement flag.Value
+func (d *Duration) Set(v string) error {
+	parsed, err := time.ParseDuration(v)
+	if err != nil {
+		return err
+	}
+	*d = Duration(parsed)
+	return nil
+}
+
+func (d *Duration) String() string {
+	duration := time.Duration(*d)
+	return duration.String()
+}
+
+func (_ SessionsLock) Name() string { return "lock l" }
+func (_ SessionsLock) Desc() string {
+	return "Установить блокировку соединений"
 }
